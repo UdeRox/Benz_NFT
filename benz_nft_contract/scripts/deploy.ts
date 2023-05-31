587ffd0b7ce26eefe31f3751cf5c528e656447bb
udeshika <perera.n.u@gmail.com>
2023-05-31 09:38:38 +0800

    contract changes

---
+++ benz_nft_contract/scripts/deploy.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+  const lockedAmount = ethers.utils.parseEther("0.001");
+  const cost = ethers.utils.parseEther("0.05");
+  const validityPeriodInDays = 30; // Set the desired validity period in days
+
+  const BenzToken = await ethers.getContractFactory("BenzToken");
+  const token = await BenzToken.deploy(cost, validityPeriodInDays);
+
+  await token.deployed();
+
+  console.log(
+    `BenzToken deployed with cost ${ethers.utils.formatEther(cost)}, validity period ${validityPeriodInDays} days, and unlock timestamp ${unlockTime} to address: ${token.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
