672e31f82d5bb21f3c7aa7a50c47b402c73a6b57
udeshika <perera.n.u@gmail.com>
2023-06-06 07:04:52 +0800

    update frontend changes

---
+++ benz_nft_ui/src/components/NftNotification.tsx
@@ -0,0 +1,32 @@
+import { SerializedError } from "@reduxjs/toolkit";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
+import { Alert, AlertTitle, AlertColor } from "../lib/mui";
+import { GoResponse } from "./types";
+export const NftNotification = ({
+  goResponse,
+  type,
+}: {
+  goResponse: GoResponse | FetchBaseQueryError | SerializedError;
+  type: AlertColor;
+}) => {
+  let data = "";
+  let status = undefined;
+
+  if ("status" in goResponse && "data" in goResponse) {
+    // goResponse is of type GoResponse
+    const { status: goResponseStatus, data: goResponseData } = goResponse;
+    data = goResponseData;
+    status = goResponseStatus;
+  } else if ("error" in goResponse) {
+    // goResponse is of type FetchBaseQueryError or SerializedError
+    data = goResponse.error;
+  } else {
+    // Unknown type
+    console.log("Invalid goResponse");
+  }
+  return (
+    <Alert severity={type}>
+      <AlertTitle>{`${status ?? ""} - ${JSON.stringify(data)}`}</AlertTitle>
+    </Alert>
+  );
+};
