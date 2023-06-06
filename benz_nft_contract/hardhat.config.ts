14d2bcbeefa969a1d95af591d49cd42393aa813c
udeshika <perera.n.u@gmail.com>
2023-06-07 06:48:46 +0800

    updated hardhat version

--- benz_nft_contract/hardhat.config.ts
+++ benz_nft_contract/hardhat.config.ts
@@ -1,13 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
-/** @type import('hardhat/config').HardhatUserConfig */
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  networks:{
+  networks: {
     hardhat: {
-        chainId: 1337
-      },
-  }
+      chainId: 1337,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+    mumbai: {
+      url: process.env.MUMBAI_TESTNET_URL,
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+  },
 };
 
 export default config;
